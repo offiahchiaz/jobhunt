66d487f4ba5e9b56a69bcd35c53981d3fb22518d
offiahchiaz <offiahchiaz@gmail.com>
2020-07-12 00:33:04 +0100

    Updated index blade

--- routes/web.php
+++ routes/web.php
@@ -11,9 +11,13 @@
 |
 */
 
-Route::get('/', function () {
-    return view('welcome');
-});
+// Route::get('/', function () {
+//     return view('welcome');
+// });
+
+// Pages Routes
+
+Route::get('/', 'PagesController@index');
 
 Auth::routes();
 
