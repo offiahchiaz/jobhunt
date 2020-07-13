6f6b079150aad7c6cf6bfeff67a53730affe5487
offiahchiaz <offiahchiaz@gmail.com>
2020-07-13 15:40:16 +0000

    Updated verify blade

--- resources/views/auth/verify.blade.php
+++ resources/views/auth/verify.blade.php
@@ -1,7 +1,43 @@
 @extends('layouts.app')
 
 @section('content')
-<div class="container">
+
+    <div class="hero-wrap hero-wrap-2" style="background-image: url('images/bg_1.jpg'); max-height:200px;" data-stellar-background-ratio="0.5">
+      <div class="overlay"></div>
+      <div class="container">
+        <div class="row no-gutters slider-text align-items-end justify-content-start">
+          <div class="col-md-12 ftco-animate text-center">
+          	{{-- <p class="breadcrumbs mb-0"><span class="mr-3"><a href="index.html">Home <i class="ion-ios-arrow-forward"></i></a></span> <span>About</span></p>
+            <h1 class="mb-3 bread">Browse Jobs</h1> --}}
+          </div>
+        </div>
+      </div>
+    </div>
+
+    <section class="ftco-section bg-light">
+        <div class="container">
+            <div class="row justify-content-center">
+                <div class="col-md-8">
+                    <div class="card">
+                        <div class="card-header">{{ __('Verify Your Email Address') }}</div>
+
+                        <div class="card-body">
+                            @if (session('resent'))
+                                <div class="alert alert-success" role="alert">
+                                    {{ __('A fresh verification link has been sent to your email address.') }}
+                                </div>
+                            @endif
+
+                            {{ __('Before proceeding, please check your email for a verification link.') }}
+                            {{ __('If you did not receive the email') }}, <a href="{{ route('verification.resend') }}">{{ __('click here to request another') }}</a>.
+                        </div>
+                    </div>
+                </div>
+            </div>
+        </div>
+    </section>
+
+{{-- <div class="container">
     <div class="row justify-content-center">
         <div class="col-md-8">
             <div class="card">
@@ -20,5 +56,6 @@
             </div>
         </div>
     </div>
-</div>
+</div> --}}
+
 @endsection
