3c4ce95b5b790b89dcc6dbe6870322d55fcaf9a6
offiahchiaz <offiahchiaz@gmail.com>
2020-07-14 15:51:50 +0000

    Added Profile model and table

---
+++ app/Profile.php
@@ -0,0 +1,17 @@
+<?php
+
+namespace App;
+
+use Illuminate\Database\Eloquent\Model;
+
+class Profile extends Model
+{
+    protected $fillable = [
+        'title', 'description', 'image'
+    ];
+
+    public function user()
+    {
+        return $this->belongsTo(User::class);
+    }
+}
