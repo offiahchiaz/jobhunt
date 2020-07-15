fa2e79f7e8e1935b0e7ef80261c1abfd8566da18
offiahchiaz <offiahchiaz@gmail.com>
2020-07-15 15:53:01 +0000

    Added ProfilesController

---
+++ app/Http/Controllers/ProfilesController.php
@@ -0,0 +1,36 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use App\User;
+use Illuminate\Http\Request;
+
+class ProfilesController extends Controller
+{
+    /**
+     * Create a new controller instance.
+     *
+     * @return void
+     */
+    public function __construct()
+    {
+        $this->middleware('auth');
+    }
+
+    /**
+     * Show the application dashboard.
+     *
+     * @return \Illuminate\Contracts\Support\Renderable
+     */
+    public function index()
+    {
+        return view('home');
+    }
+
+    public function show($user)
+    {
+        // $user = User::find($user);
+
+        return view('home');
+    }
+}
